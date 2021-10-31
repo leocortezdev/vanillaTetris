27be5021b0c06055b0296f0a487e0caa49b4ac1c
LCFirm <leo.cortez55@gmail.com>
2021-10-31 18:05:07 -0400

    Setup Canvas/Basic tetris motions

--- tetris.js
+++ tetris.js
@@ -12,6 +12,26 @@ const matrix = [
   [0, 1, 0],
 ];
 
+const collide = (arena, player) => {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; y++) {
+    for (let x = 0; x < m[y].length; x++) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const createMatrix = (w, h) => {
+  const matrix = [];
+  while (h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+};
+
 const draw = () => {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -29,8 +49,23 @@ const drawMatrix = (matrix, offset) => {
   });
 };
 
+const merge = (arena, player) => {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+};
+
 const playerDrop = () => {
   player.pos.y++;
+  if(collide(arena, player)) {
+      player.pos.y--;
+      merge(arena, player);
+      player.pos.y = 0;
+  } 
   dropCounter = 0;
 };
 
@@ -47,6 +82,10 @@ const update = (time = 0) => {
   draw();
   requestAnimationFrame(update);
 };
+
+const arena = createMatrix(12, 20);
+console.log(arena);
+
 
 const player = {
   pos: { x: 5, y: 5 },
